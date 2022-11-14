0f6b1b2ac303413ad3da7d1895644e62c5ea8214
TheHuntsman4 <anikethvij464@gmail.com>
2022-11-14 23:43:43 +0530

    redo

---
+++ task-08/task-08/index.js
@@ -0,0 +1,23 @@
+function sound1(){
+    var audio =new Audio('sounds/crash.mp3');
+    audio.play();
+};
+function sound2(){
+    var audio =new Audio('sounds/tom-4.mp3');
+    audio.play();
+};function sound3(){
+    var audio =new Audio('sounds/kick-bass.mp3');
+    audio.play();
+};function sound4(){
+    var audio =new Audio('sounds/snare.mp3');
+    audio.play();
+};function sound5(){
+    var audio =new Audio('sounds/tom-1.mp3');
+    audio.play();
+};function sound6(){
+    var audio =new Audio('sounds/tom-2.mp3');
+    audio.play();
+};function sound7(){
+    var audio =new Audio('sounds/tom-3.mp3');
+    audio.play();
+};
