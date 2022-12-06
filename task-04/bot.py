d4f2707e18c7c3c02a3961938df759732a2dd047
TheHuntsman4 <anikethvij464@gmail.com>
2022-12-07 00:36:16 +0530

    update tele

--- task-04/bot.py
+++ task-04/bot.py
@@ -25,7 +25,7 @@ def getmovie(name):
     urlData=urlData.json()
     title,year,rating,photo=urlData["Title"],urlData["Year"],urlData["imdbRating"],urlData["Poster"]
     output=f'{photo}\nTitle:{title}\nYear:{year}\nimDbRating:{rating}'
-    bot.send_message(message.chat.id,output)
+    # bot.send_message(message.chat.id,output)
     csvExport([title,year,rating])
     return output
 
